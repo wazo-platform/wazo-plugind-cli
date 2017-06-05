32fa10b4339de07f5b4d23aee216ed2af8bc0440
Pascal Cadotte Michaud <pcm@wazo.community>
2017-06-05 12:09:48 -0400

    refactor: move command implementations to the command module

---
+++ wazo_plugind_cli/command.py
@@ -0,0 +1,77 @@
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import kombu
+
+from xivo.cli import BaseCommand, UsageError
+from .bus import ProgressConsumer
+
+
+class _BasePlugindCommand(BaseCommand):
+
+    def __init__(self, client):
+        super().__init__()
+        self._client = client
+
+    def execute(self):
+        raise NotImplementedError()
+
+
+class _BaseAsyncCommand(_BasePlugindCommand):
+
+    def __init__(self, plugind_client, config):
+        super().__init__(plugind_client)
+        self.amqp_url = 'amqp://{username}:{password}@{host}:{port}//'.format(**config['bus'])
+        self.exchange = kombu.Exchange(config['bus']['exchange_name'],
+                                       config['bus']['exchange_type'])
+
+    def execute(self, *args):
+        async = args[-1]
+        if async:
+            self.execute_async(*args[:-1])
+        else:
+            self.execute_sync(*args[:-1])
+
+    def execute_sync(self, *args):
+        result = self.execute_async(*args)
+        routing_key = self.routing_key_fmt.format(result['uuid'])
+
+        with kombu.Connection(self.amqp_url) as conn:
+            ProgressConsumer(conn, routing_key, self.exchange).run()
+
+
+class InstallCommand(_BaseAsyncCommand):
+
+    help = 'Install a plugin'
+    usage = '<method> <plugin> [--async]'
+    routing_key_fmt = 'plugin.install.{}.*'
+
+    def prepare(self, command_args):
+        try:
+            method = command_args[0]
+            plugin = command_args[1]
+            async = len(command_args) > 2 and command_args[2] == '--async'
+            return method, plugin, async
+        except Exception:
+            raise UsageError()
+
+    def execute_async(self, method, plugin):
+        return self._client.plugins.install(plugin, method)
+
+
+class UninstallCommand(_BaseAsyncCommand):
+
+    help = 'Uninstall a plugin'
+    usage = '<namespace>/<name> [--async]'
+    routing_key_fmt = 'plugin.uninstall.{}.*'
+
+    def prepare(self, command_args):
+        try:
+            namespace, name = command_args[0].split('/', 1)
+            async = len(command_args) > 1 and command_args[1] == '--async'
+            return namespace, name, async
+        except Exception:
+            raise UsageError()
+
+    def execute_async(self, namespace, name):
+        return self._client.plugins.uninstall(namespace, name)
--- wazo_plugind_cli/main.py
+++ wazo_plugind_cli/main.py
@@ -3,14 +3,13 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 import sys
-import kombu
 
-from xivo.cli import BaseCommand, Interpreter, UsageError
+from xivo.cli import Interpreter
 from xivo.token_renewer import TokenRenewer
 from wazo_plugind_client import Client as PlugindClient
 from xivo_auth_client import Client as AuthClient
 from .config import load_config
-from .bus import ProgressConsumer
+from . import command
 
 
 def _new_auth_client(config):
@@ -29,86 +28,16 @@ def main():
 
     interpreter = Interpreter(prompt='wazo-plugind-cli> ',
                               history_file='~/.wazo_plugind_cli_history')
-    interpreter.add_command('install', InstallCommand(plugind_client, config))
-    interpreter.add_command('uninstall', UninstallCommand(plugind_client, config))
+    interpreter.add_command('install', command.InstallCommand(plugind_client, config))
+    interpreter.add_command('uninstall', command.UninstallCommand(plugind_client, config))
 
     token_renewer.subscribe_to_token_change(plugind_client.set_token)
-    command = config.get('command')
+    command_name = config.get('command')
     with token_renewer:
-        if command:
-            interpreter.execute_command_line(command)
+        if command_name:
+            interpreter.execute_command_line(command_name)
         else:
             interpreter.loop()
-
-
-class BasePlugindCommand(BaseCommand):
-
-    def __init__(self, client):
-        super().__init__()
-        self._client = client
-
-    def execute(self):
-        raise NotImplementedError()
-
-
-class BaseAsyncCommand(BasePlugindCommand):
-
-    def __init__(self, plugind_client, config):
-        super().__init__(plugind_client)
-        self.amqp_url = 'amqp://{username}:{password}@{host}:{port}//'.format(**config['bus'])
-        self.exchange = kombu.Exchange(config['bus']['exchange_name'],
-                                       config['bus']['exchange_type'])
-
-    def execute(self, *args):
-        async = args[-1]
-        if async:
-            self.execute_async(*args[:-1])
-        else:
-            self.execute_sync(*args[:-1])
-
-    def execute_sync(self, *args):
-        result = self.execute_async(*args)
-        routing_key = self.routing_key_fmt.format(result['uuid'])
-
-        with kombu.Connection(self.amqp_url) as conn:
-            ProgressConsumer(conn, routing_key, self.exchange).run()
-
-
-class InstallCommand(BaseAsyncCommand):
-
-    help = 'Install a plugin'
-    usage = '<method> <plugin> [--async]'
-    routing_key_fmt = 'plugin.install.{}.*'
-
-    def prepare(self, command_args):
-        try:
-            method = command_args[0]
-            plugin = command_args[1]
-            async = len(command_args) > 2 and command_args[2] == '--async'
-            return method, plugin, async
-        except Exception:
-            raise UsageError()
-
-    def execute_async(self, method, plugin):
-        return self._client.plugins.install(plugin, method)
-
-
-class UninstallCommand(BaseAsyncCommand):
-
-    help = 'Uninstall a plugin'
-    usage = '<namespace>/<name> [--async]'
-    routing_key_fmt = 'plugin.uninstall.{}.*'
-
-    def prepare(self, command_args):
-        try:
-            namespace, name = command_args[0].split('/', 1)
-            async = len(command_args) > 1 and command_args[1] == '--async'
-            return namespace, name, async
-        except Exception:
-            raise UsageError()
-
-    def execute_async(self, namespace, name):
-        return self._client.plugins.uninstall(namespace, name)
 
 
 if __name__ == '__main__':
