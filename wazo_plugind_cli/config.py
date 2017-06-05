899fa28f17d57edea5f6d239a24a5ddec0f7361e
Pascal Cadotte Michaud <pcm@wazo.community>
2017-06-05 11:51:43 -0400

    refactor: move all configuration related code to the config module

---
+++ wazo_plugind_cli/config.py
@@ -0,0 +1,53 @@
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import argparse
+from xivo.chain_map import ChainMap
+from xivo.config_helper import parse_config_file
+
+_CERT_FILE = '/usr/share/xivo-certs/server.crt'
+_DEFAULT_CONFIG = {
+    'auth': {
+        'host': 'localhost',
+        'key_file': '/var/lib/xivo-auth-keys/wazo-plugind-cli-key.yml',
+        'verify_certificate': _CERT_FILE,
+    },
+    'plugind': {
+        'host': 'localhost',
+        'verify_certificate': _CERT_FILE,
+    },
+    'bus': {
+        'username': 'guest',
+        'password': 'guest',
+        'host': 'localhost',
+        'port': 5672,
+        'exchange_name': 'xivo',
+        'exchange_type': 'topic',
+    },
+}
+
+
+def load_config(argv):
+    cli_config = _parse_cli_args(argv)
+    key_config = _load_key_file(ChainMap(cli_config, _DEFAULT_CONFIG))
+    return ChainMap(cli_config, key_config, _DEFAULT_CONFIG)
+
+
+def _parse_cli_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c',
+                        '--command',
+                        action='store',
+                        help='Command to run.')
+    parsed_args = parser.parse_args(argv)
+    result = {}
+    if parsed_args.command:
+        result['command'] = parsed_args.command
+
+    return result
+
+
+def _load_key_file(config):
+    key_file = parse_config_file(config['auth']['key_file'])
+    return {'auth': {'service_id': key_file['service_id'],
+                     'service_key': key_file['service_key']}}
--- wazo_plugind_cli/main.py
+++ wazo_plugind_cli/main.py
@@ -2,64 +2,15 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-import argparse
 import sys
 import kombu
 
 from kombu.mixins import ConsumerMixin
-from xivo.chain_map import ChainMap
 from xivo.cli import BaseCommand, Interpreter, UsageError
-from xivo.config_helper import parse_config_file
 from xivo.token_renewer import TokenRenewer
 from wazo_plugind_client import Client as PlugindClient
 from xivo_auth_client import Client as AuthClient
-
-_CERT_FILE = '/usr/share/xivo-certs/server.crt'
-_DEFAULT_CONFIG = {
-    'auth': {
-        'host': 'localhost',
-        'key_file': '/var/lib/xivo-auth-keys/wazo-plugind-cli-key.yml',
-        'verify_certificate': _CERT_FILE,
-    },
-    'plugind': {
-        'host': 'localhost',
-        'verify_certificate': _CERT_FILE,
-    },
-    'bus': {
-        'username': 'guest',
-        'password': 'guest',
-        'host': 'localhost',
-        'port': 5672,
-        'exchange_name': 'xivo',
-        'exchange_type': 'topic',
-    },
-}
-
-
-def load_config(argv):
-    cli_config = _parse_cli_args(argv)
-    key_config = _load_key_file(ChainMap(cli_config, _DEFAULT_CONFIG))
-    return ChainMap(cli_config, key_config, _DEFAULT_CONFIG)
-
-
-def _parse_cli_args(argv):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c',
-                        '--command',
-                        action='store',
-                        help='Command to run.')
-    parsed_args = parser.parse_args(argv)
-    result = {}
-    if parsed_args.command:
-        result['command'] = parsed_args.command
-
-    return result
-
-
-def _load_key_file(config):
-    key_file = parse_config_file(config['auth']['key_file'])
-    return {'auth': {'service_id': key_file['service_id'],
-                     'service_key': key_file['service_key']}}
+from .config import load_config
 
 
 def _new_auth_client(config):
